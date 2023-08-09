46b6d211e462bee5108f16fbb70c615daccc718e
KoYunSeok <a01021719359@gmail.com>
2023-08-09 12:39:38 +0900

    zipkin sleuth 관련 코드 추가

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -8,11 +8,13 @@ import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -21,6 +23,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/order-service")
 public class OrderController {
     private final Environment environment;
@@ -37,6 +40,7 @@ public class OrderController {
     @PostMapping("/{userId}/orders")
     public ResponseEntity <ResponseOrder> createdOrder (@PathVariable("userId") String userId,
                                                     @RequestBody RequestOrder requestOrder ){
+        log.info ( "Before add orders data" );
         ModelMapper modelMapper = new ModelMapper ( );
         modelMapper.getConfiguration ( ).setMatchingStrategy ( MatchingStrategies.STRICT );
 
@@ -52,15 +56,25 @@ public class OrderController {
 //        kafkaProducer.send ( "example-catalog-topic", dto );
 //        orderProducer.send ( "orders", dto );
 //        ResponseOrder responseUser = modelMapper.map ( dto, ResponseOrder.class );
-
+        log.info ( "After add orders data" );
         return ResponseEntity.status ( HttpStatus.CREATED ).body ( responseUser );
     }
 
     @GetMapping("/{userId}/orders")
-    public ResponseEntity<List <ResponseOrder>> getOrders (@PathVariable("userId") String userId ){
+    public ResponseEntity<List <ResponseOrder>> getOrders (@PathVariable("userId") String userId ) throws Exception {
+        log.info ( "Before receive orders data" );
         Iterable<OrderEntity> userList = orderService.getOrderByUserId ( userId );
         List<ResponseOrder> result = new ArrayList <> ();
         userList.forEach ( item -> result.add ( new ModelMapper ().map ( item, ResponseOrder.class ) ) );
+
+        try{
+            Thread.sleep ( 1000 );
+            throw new Exception ("장애 발생");
+        }catch ( InterruptedException ex ){
+            log.warn ( ex.getMessage () );
+        }
+
+        log.info ( "After receive orders data" );
         return ResponseEntity.status ( HttpStatus.OK ).body ( result );
     }
 }
